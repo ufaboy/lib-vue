a8f2fe061e7d8e1ed39b7b5de96dcd83c66431b1
AzaT <39663655+ufaboy@users.noreply.github.com>
2021-08-03 18:58:52 +0500

    Update LayoutDefault.vue

--- src/layouts/LayoutDefault.vue
+++ src/layouts/LayoutDefault.vue
@@ -2,7 +2,7 @@
   <div class="basement">
 <!--    <canvas-space :count-dots="countDots"/>-->
     <the-sunrise/>
-    <header v-if="$route.name !== 'book-view'" class="header" :class="{'header--dark': $route.name === 'book-view'}">
+    <header v-if="showHeader" class="header" :class="{'header--dark': $route.name === 'book-view'}">
       <div class="header--left">
         <router-link class="breadcrumb-home" to="/">Home</router-link>
         <ul class="breadcrumb" v-if="isDesktop">
@@ -89,6 +89,9 @@ export default {
     }
   },
   computed: {
+    showHeader() {
+      return this.$route.name === 'book-view' ? !this.isMobile : true
+    },
     categories() {
       return this.$store.state.genre.categories
     },
