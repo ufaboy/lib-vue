98bb28d7482a6d936b74e2b7cd7e55ac6cd774b5
Azat Kashapov <azat.developer@gmail.com>
2022-06-28 21:30:26 +0500

    rework pwa icons

--- vite.config.ts
+++ vite.config.ts
@@ -27,71 +27,18 @@ export default ({mode}) => {
                     background_color: '#004e92',
                     icons: [
                         {
-                            src: "./icons/pwa/maskable_icon_x48.png",
-                            sizes: "48x48",
-                            type: "image/png",
-                            purpose: "maskable"
-                        },
-                        {
-                            src: "./icons/pwa/maskable_icon_x96.png",
-                            sizes: "96x96",
-                            type: "image/png",
-                            purpose: "maskable"
-                        },
-                        {
-                            src: "./icons/pwa/maskable_icon_x72.png",
-                            sizes: "72x72",
-                            type: "image/png",
-                            purpose: "maskable"
-                        },
-                        {
-                            src: "./icons/pwa/maskable_icon_x128.png",
-                            sizes: "128x128",
-                            type: "image/png",
-                            purpose: "maskable"
-                        },
-                        {
-                            src: "./icons/pwa/maskable_icon_x144.png",
-                            sizes: "144x144",
-                            type: "image/png",
-                            purpose: "maskable"
-                        },
-                        {
-                            src: "./icons/pwa/regular_icon_x144.png",
-                            sizes: "144x144",
-                            type: "image/png",
-                            purpose: "any"
-                        },
-                        {
-                            src: "./icons/pwa/maskable_icon_x192.png",
+                            src: "icon-192.png",
                             sizes: "192x192",
                             type: "image/png",
                             purpose: "maskable"
                         },
                         {
-                            src: "./icons/pwa/maskable_icon_x192.png",
-                            sizes: "192x192",
-                            type: "image/png",
-                            purpose: "any"
-                        },
-                        {
-                            src: "./icons/pwa/maskable_icon_x384.png",
-                            sizes: "384x384",
-                            type: "image/png",
-                            purpose: "maskable"
-                        },
-                        {
-                            src: "./icons/pwa/maskable_icon_x512.png",
+                            src: "/icon-512.png",
                             sizes: "512x512",
                             type: "image/png",
                             purpose: "maskable"
                         },
-                        {
-                            src: "./icons/pwa/regular_icon_x512.png",
-                            sizes: "512x512",
-                            type: "image/png",
-                            purpose: "any"
-                        }
+
                     ],
                 }
             })],
